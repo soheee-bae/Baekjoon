9dce7bc1a2049d94a6e231eb9d26fb855cd8b15c
sohee28 <amybae28@gmail.com>
2023-07-05 22:38:01 -0500

    solving problem

--- Baekjoon/Bronze III/3009.js
+++ Baekjoon/Bronze III/3009.js
@@ -1,2 +1,23 @@
 // 네번째점
 
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(line.split(" ").map((el) => parseInt(el)));
+}).on("close", function () {
+  let xs = input.map((x) => x[0]).sort((a, b) => a - b);
+  let ys = input.map((y) => y[1]).sort((a, b) => a - b);
+
+  const x = xs[0] === xs[1] ? xs[2] : xs[0];
+  const y = ys[0] === ys[1] ? ys[2] : ys[0];
+
+  console.log(x, y);
+  process.exit();
+});
