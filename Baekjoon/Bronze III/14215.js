160280e24c46b5f6b21ca39fad4a6d5e6cddd7fa
sohee28 <amybae28@gmail.com>
2023-07-07 00:21:49 -0500

    complete 14215

--- Baekjoon/Bronze III/14215.js
+++ Baekjoon/Bronze III/14215.js
@@ -1,1 +1,28 @@
 // 세 막대
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ").map((el) => parseInt(el));
+}).on("close", function () {
+  let result = 0;
+  const sortedInput = input.sort((a, b) => a - b);
+
+  const [A, B, C] = sortedInput;
+  const set = new Set(sortedInput);
+
+  if (set.size === 1 || C < A + B) {
+    result = A + B + C;
+  } else {
+    result = (A + B) * 2 - 1;
+  }
+  console.log(result);
+  process.exit();
+});
