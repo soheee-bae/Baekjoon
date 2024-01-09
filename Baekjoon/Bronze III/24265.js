97ce08b6c5bb33d2e5120ab57d2eb0e68c8ba331
sohee28 <amybae28@gmail.com>
2024-01-09 21:52:02 +0900

    problem solving

--- Baekjoon/Bronze III/24265.js
+++ Baekjoon/Bronze III/24265.js
@@ -1,2 +1,23 @@
 //	알고리즘 수업 - 알고리즘의 수행 시간 4
 
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+
+rl.on("line", function (line) {
+  input = line;
+  rl.close();
+}).on("close", function () {
+  let sum = 0;
+
+  for (let i = 1; i <= input - 1; i++) {
+    sum += i;
+  }
+  console.log(sum);
+  console.log(2);
+});
