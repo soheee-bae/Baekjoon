6e8b000599387d4dca64c542256c00154c39e1b5
sohee28 <amybae28@gmail.com>
2024-01-17 17:35:09 +0900

    clean

--- Baekjoon/Bronze V/A+B.js
+++
@@ -1,8 +0,0 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
-let total = a + b;
-
-console.log(total);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Baekjoon Algorithm
-🔎 백준 코딩테스트 알고리즘 공부 기록
+## Baekjoon Algorithm
+
+
 
